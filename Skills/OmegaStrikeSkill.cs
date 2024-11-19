9cb1e346f80ee59ead4b1762a42e5fc671b033e3
rainwalk <tioyou@hotmail.com>
2024-11-19 13:25:54 +0900

    Delete Skills directory

--- Skills/OmegaStrikeSkill.cs
+++
@@ -1,7 +0,0 @@
-public class OmegaStrikeSkill : SkillBase
-{
-    public OmegaStrikeSkill() : base("오메가 스트라이크", SkillTargetType.Single, 5f, 1f) {
-        AddEffect(new DamageEffect(90)); 
-    }
-
-}
--- Skills/PlasmaRainSkill.cs
+++
@@ -1,9 +0,0 @@
-using System.Collections.Generic;
-using UnityEngine;
-
-public class PlasmaRainSkill : SkillBase
-{
-    public PlasmaRainSkill() : base("플라즈마 레인", SkillTargetType.All, 10f, 3f) {
-        AddEffect(new DamageEffect(40));
-    }
-}
